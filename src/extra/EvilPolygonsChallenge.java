eb2a95ee9b3656355b5bfd2ebdb1d88c49422911
60222 <60222@0338S-S-R0B6ZHH.sdcs.local>
2019-04-23 14:25:52 -0700

    4/23/19

--- src/extra/EvilPolygonsChallenge.java
+++ src/extra/EvilPolygonsChallenge.java
@@ -10,17 +10,60 @@ public class EvilPolygonsChallenge {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-
+		Robot lul = new Robot ();
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
-		
+		lul.setSpeed(100);
+		int colorChoice=JOptionPane.showOptionDialog(null, "What color do you want?", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Blue","Red", "Black","Green","Yellow"}, 0);
+		System.out.println();
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
+		if (colorChoice == 2) {
+			lul.setPenColor(255,255,255);
+			lul.penDown();
+		}
+		if (colorChoice == 0) {
+			lul.setPenColor(0,0,255);
+			lul.penDown();
+		}
+		if (colorChoice == 1) {
+			lul.setPenColor(255,0,0);
+			lul.penDown();
+		}
+		if (colorChoice == 3) {
+			lul.setPenColor(0,255,0);
+			lul.penDown();
+		}
+		if (colorChoice ==4) {
+			lul.setPenColor(255,255,0);
+			lul.penDown();
+		}
 		//4. Ask the use how many polygons they want to be drawn.
-		
+		String poly = JOptionPane.showInputDialog("How many polygons do you want to draw?");
 		//5. Use the robot to draw the number of polygons the user requested.
-		
+		for (int i = 0; i < 2; i++) {
+		lul.turn(75);
+		lul.move(100);
+		lul.turn(-150);
+		lul.move(100);
+		lul.turn(-105);
+		lul.move(50);
+	}
+		for (int i = 0; i < 2; i++) {
+		lul.turn(90);
+		lul.move(75);;
+		lul.move(90);
+		lul.turn(45);
+		lul.move(100);
+		lul.turn(45);
+		lul.move(150);
+		lul.turn(90);
+		lul.move(200);
+		lul.turn(90);
+		lul.move(150);
+		lul.turn(45);
+		lul.move(100);
+	}
+		lul.penDown();
+	
 		//6. Make it so your shapes do not overlap
 
 		//7. Challenge: add more colors to the Option Dialog.
--- src/nested_loops/CircleRing.java
+++ src/nested_loops/CircleRing.java
@@ -10,26 +10,33 @@ public class CircleRing {
     public static void main(String[] args) throws Exception {
 
         // 1. Make a new Robot
-
+    	Robot hehe = new Robot ();
 
        //  2. Set your robot’s position to x=150 and y=200
-
+    	hehe.setX(150);
+    	hehe.setY(200);
         // 3. Put the robot's pen down
-
+    	hehe.penDown();
         // 4. Set the robot’s speed to 10
-
+    	hehe.setSpeed(40);
         // 5. Do everything below here 360 times (use i as the counter)
-
+    	for (int i = 0; i < 360; i++) {
+			
 	        // 6. Move the robot 3 pixels
-
+    		hehe.move(3);
        //  7. Turn the robot 1 degree
-
+    		hehe.turn(1);
         // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
-
+    		if (i %20 == 0) {
+    			for (int j = 0; j < 360; j++) {
+				hehe.move(1);
+    			hehe.turn(1);
+    			}
+    		}
 		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
 
 				// 10. Move the robot 1 pixel
-
-				// 11. Turn the robot 1 degree
+    			// 11. Turn the robot 1 degree
+    	}
 }
 }
