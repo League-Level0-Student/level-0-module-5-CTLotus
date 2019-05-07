0b8276657eebc7b1384b6391171fc1bc7d38c65e
60222 <60222@0338S-S-R0B6ZHH.sdcs.local>
2019-05-07 14:25:26 -0700

    5/7/19

---
+++ src/algorithms/PrimeOrNot.java
@@ -0,0 +1,40 @@
+package algorithms;
+
+import javax.swing.JOptionPane;
+
+public class PrimeOrNot {
+	public static void main(String[] args) {
+		String user = JOptionPane.showInputDialog("Pick a number. Any number..");
+		String confirm = JOptionPane.showInputDialog("Is the number a prime number?");
+		
+		boolean prime = true;
+		for (int i = 2; i < 100; i++) {
+			if(100%i == 0) {
+				prime = false;
+		}
+		
+		if (prime ==true) {
+			
+		}else {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
--- src/nested_loops/NestedForLoops.java
+++ src/nested_loops/NestedForLoops.java
@@ -4,15 +4,28 @@ public class NestedForLoops {
 	public static void main(String[] args) {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
-				System.out.println(i + ""+ j);
+				System.out.print(i*3 + j + 1);
 			}
-		}
-		for (int o = 0; o < 4; o++) {
-			for (int i = 4; i < 7; i++) {
-				System.out.println();
-			}
+			System.out.println();
 		}
 		
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.print(i*10 + j + 1);
+			}
+			System.out.println();
+		}
+		
+		for (int i = 0; i <= 6; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 100; i > -1; i--) {
+			System.out.println(i);
+		}
 		
 		
 		
